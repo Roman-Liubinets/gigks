f9870c178ca0570552a66eea34a3c27503dc8d62
Roman Liubinets <beluy95@ukr.net>
2017-11-04 15:53:17 +0200

    in script.js wrote script for buttons in navigation menu

--- public/script/script.js
+++ public/script/script.js
@@ -1,6 +1,6 @@
 var app = angular.module('app', ['ngDialog']);
 
-app.controller("myCtrl", function ($scope, $http, ngDialog) {});
+app.controller("myCtrl", function($scope, $http, ngDialog) {});
 
 //Директива панелі меню
 app.directive("headerBlock", function() {
@@ -8,12 +8,74 @@ app.directive("headerBlock", function() {
         replace: true,
         templateUrl: "template/header.html",
         controller: function($scope, $http) {
-            $scope.home =true;
+            $scope.home = true;
             $scope.video = false;
             $scope.contact = false;
             $scope.simulators = false;
             $scope.school = false;
             $scope.userBlock = false;
+
+            $scope.menuNavBtn = [{
+                name: "Головна",
+                action: function() {
+                    $scope.home = true;
+                    $scope.video = false;
+                    $scope.contact = false;
+                    $scope.simulators = false;
+                    $scope.school = false;
+                    $scope.userBlock = false;
+                }
+            }, {
+                name: "Відео",
+                action: function() {
+                    $scope.home = false;
+                    $scope.video = true;
+                    $scope.contact = false;
+                    $scope.simulators = false;
+                    $scope.school = false;
+                    $scope.userBlock = false;
+                }
+            }, {
+                name: "Контакти",
+                action: function() {
+                    $scope.home = false;
+                    $scope.video = false;
+                    $scope.contact = true;
+                    $scope.simulators = false;
+                    $scope.school = false;
+                    $scope.userBlock = false;
+                }
+            }, {
+                name: "Тренажери",
+                action: function() {
+                    $scope.home = false;
+                    $scope.video = false;
+                    $scope.contact = false;
+                    $scope.simulators = true;
+                    $scope.school = false;
+                    $scope.userBlock = false;
+                }
+            }, {
+                name: "Школа",
+                action: function() {
+                    $scope.home = false;
+                    $scope.video = false;
+                    $scope.contact = false;
+                    $scope.simulators = false;
+                    $scope.school = true;
+                    $scope.userBlock = false;
+                }
+            }, {
+                name: "Школа",
+                action: function() {
+                    $scope.home = false;
+                    $scope.video = false;
+                    $scope.contact = false;
+                    $scope.simulators = false;
+                    $scope.school = false;
+                    $scope.userBlock = true;
+                }
+            }]
         }
     }
 });
