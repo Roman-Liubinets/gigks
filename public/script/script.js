4a9b43798a9ee2ab49ea8752703c428e2a4ad141
Roman Liubinets <beluy95@ukr.net>
2017-11-04 18:39:24 +0200

    done with text, done first part of styling

--- public/script/script.js
+++ public/script/script.js
@@ -108,3 +108,114 @@ app.directive("videoBlock", function() {
         controller: function($scope, $http, ngDialog) {}
     }
 });
+
+//Директива для слайдера
+app.directive("sliderBlock", function() {
+    return {
+        replace: true,
+        templateUrl: "template/slider.html",
+        controller: function($scope, $http, ngDialog) {
+            var slideNow = 1;
+            var translateWidth = 0;
+            var slideCount = $('#slidewrapper').children().length;
+
+            function nextSlide() {
+                if (slideNow == 2) {
+                    $("#slidewrapper li:nth-child(3)").css({
+                        "display": "inline"
+                    })
+                } else if (slideNow == 3) {
+                    $("#slidewrapper li:nth-child(4)").css({
+                        "display": "inline"
+                    })
+                }
+                if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+                    $("#slidewrapper").css('transform', 'translate(0, 0)');
+                    slideNow = 1;
+                } else {
+                    translateWidth = -$('#viewport').width() * (slideNow);
+                    $('#slidewrapper').css({
+                        'transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                    });
+                    slideNow++;
+                }
+            }
+
+            var slideInterval = 2000;
+
+            // $(document).ready(function() {
+            //     var switchInterval = setInterval(nextSlide, slideInterval);
+            //
+            //     $('#viewport').hover(function() {
+            //         clearInterval(switchInterval);
+            //     }, function() {
+            //         switchInterval = setInterval(nextSlide, slideInterval);
+            //     });
+            // });
+
+            function prevSlide() {
+                if (slideNow == 1) {
+                    $("#slidewrapper li:nth-child(4)").css({
+                        "display": "inline"
+                    })
+                } else if (slideNow == 4) {
+                    $("#slidewrapper li:nth-child(3)").css({
+                        "display": "inline"
+                    })
+                }
+
+                if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+                    translateWidth = -$('#viewport').width() * (slideCount - 1);
+                    $('#slidewrapper').css({
+                        'transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                    });
+                    slideNow = slideCount;
+                } else {
+                    translateWidth = -$('#viewport').width() * (slideNow - 2);
+                    $('#slidewrapper').css({
+                        'transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                    });
+                    slideNow--;
+                }
+            }
+
+            $('#next-btn').click(function() {
+                nextSlide();
+            });
+
+            $('#prev-btn').click(function() {
+                prevSlide();
+            });
+
+            var navBtnId = 0;
+
+            $('.slide-nav-btn').click(function() {
+                navBtnId = $(this).index();
+                if (this) {
+                    $("#slidewrapper li:nth-child(3)").css({
+                        "display": "inline"
+                    })
+                    $("#slidewrapper li:nth-child(4)").css({
+                        "display": "inline"
+                    })
+                }
+
+                if (navBtnId + 1 != slideNow) {
+                    translateWidth = -$('#viewport').width() * (navBtnId);
+                    $('#slidewrapper').css({
+                        'transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+                    });
+                    slideNow = navBtnId + 1;
+                }
+            });
+        }
+    }
+});
